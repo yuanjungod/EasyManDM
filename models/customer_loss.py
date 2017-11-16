407c44776290ef705ec5bf851ddee5fe343f0108
yuanjun <yuanjun@papayamobile.com>
2017-11-16 16:41:15 +0800

    123

--- models/customer_loss.py
+++ models/customer_loss.py
@@ -16,20 +16,22 @@ class CustomerLoss(ModelBase):
     def __init__(self, num_class):
         super(CustomerLoss, self).__init__()
         self.num_classes = num_class
-        self.model.add(Conv2D(128, (2, 2), padding='same', input_shape=(3, 97, 1), activation='relu'))
-        self.model.add(MaxPooling2D(pool_size=(1, 2)))
-        self.model.add(Conv2D(64, (2, 3), padding='same', activation='relu'))
-        self.model.add(MaxPooling2D(pool_size=(1, 2)))
-        self.model.add(Conv2D(32, (2, 5), padding='valid', activation='relu'))
+        self.model.add(Conv2D(64, (2, 4), padding='same', input_shape=(3, 97, 1), activation='tanh'))
         self.model.add(MaxPooling2D(pool_size=(2, 2)))
-        self.model.add(Conv2D(15, (1, 5), padding='valid', activation='relu'))
+        self.model.add(Conv2D(32, (2, 4), padding='same', activation='tanh'))
         self.model.add(MaxPooling2D(pool_size=(1, 2)))
+        # self.model.add(Conv2D(32, (2, 4), padding='same', activation='tanh'))
+        # self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        # self.model.add(Conv2D(15, (2, 4), padding='same', activation='tanh'))
+        # self.model.add(MaxPooling2D(pool_size=(2, 2)))
         self.model.add(Flatten())
-        self.model.add(Dense(128, activation='relu'))
+        self.model.add(Dense(64, activation='relu'))
+        self.model.add(Dense(32, activation='relu'))
+        self.model.add(Dense(16, activation='relu'))
         self.model.add(Dense(self.num_classes, activation='softmax'))
         sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
         adadelta = optimizers.Adadelta(lr=1.0, rho=0.95, epsilon=1e-06)
-        self.model.compile(loss='categorical_crossentropy', optimizer=adadelta, metrics=[categorical_accuracy])
+        self.model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=[categorical_accuracy])
 
 
 
